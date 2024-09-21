b98d0b5fc6e9290d71b9d810acaace5936b8ae3b
Evangelos Manikas <53276046+ManikasEv@users.noreply.github.com>
2024-09-22 00:40:11 +0300

    update api

---
+++ api/products.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Product from '../models/product.model.js'; 
+import cors from 'cors';
+
+const router = express.Router();
+
+router.use(cors());
+router.use(express.json());
+
+router.get('/', (req, res) => {
+    res.send('Hello from Node API');
+});
+
+// Get all products
+router.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get a specific product by ID
+router.get('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Create a new product
+router.post('/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update a product
+router.put('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedProduct = await Product.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
