eac7324b3ae6b296a9dd1e3d7d6362a419a6e24e
Evangelos Manikas <53276046+ManikasEv@users.noreply.github.com>
2024-09-22 00:46:13 +0300

    update api

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Product from '../models/product.model.js';
 import cors from 'cors';
-import productRoutes from './api/products.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', productRoutes);
+let isConnected; // Track connection status
 
-mongoose.connect("mongodb+srv://6nSAv3DNQi7hQn6K:6nSAv3DNQi7hQn6K@cluster0.iwchv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => {
-    console.log("Connected to DB");
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000');
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to DB:", error);
-  });
+const connectToDatabase = async () => {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    throw error; // Throw to let the error handler catch it
+  }
+};
+
+app.use(async (req, res, next) => {
+  await connectToDatabase();
+  next();
+});
+
+// Define your routes...
+
+app.use((err, req, res, next) => {
+  console.error("Error:", err);
+  res.status(500).send('Internal Server Error');
+});
 
 export default app;
